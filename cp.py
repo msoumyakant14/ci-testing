b32de7077469d1c21fdaf207a93e3484cbaad3a0
Soumyakant Mohakul <48150731+msoumyakant14@users.noreply.github.com>
2022-04-13 12:40:00 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -7,10 +7,14 @@ def bubbleSort(arr):
                 arr[i] = arr[j]
                 arr[j] = tmp
     print(arr)
-    
+
+
 if __name__ == __main__:
     arr = []
     for i in range(10):
         arr.append(input("Enter element" + str(i+1)))
     bubbleSort(arr)
-    
+
+
+
+
